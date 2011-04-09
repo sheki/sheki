03edf3f11086402dd65d62036ee6d460b6045512
Abhishek Kona <abhishek.kona@gmail.com>
2011-04-09 23:38:36 +0530

    added comments

--- codejam/minscalarProduct.scala
+++ codejam/minscalarProduct.scala
@@ -1,3 +1,4 @@
+//Solution to code jam problem : http://goo.gl/B2tS9
 def minScalarProduct( x : List[Int],y : List[Int] ) : Long = {
 
   val sortedX = x.sortWith(_<_) 
@@ -7,18 +8,16 @@ def minScalarProduct( x : List[Int],y : List[Int] ) : Long = {
     case Seq() => 0
     case x1::xs1 => x1 * y.head + scalarProduct ( xs1, y.tail ) 
   }
-
   scalarProduct(sortedX,sortedY)
-
 }  
 
 def processInput(source: io.Source) {
   val groups = source.getLines.drop(1).grouped(3) 
 
-    groups.zipWithIndex foreach { e =>  
-  val solution = minScalarProduct ( e._1(1).split(" ").map(_.toInt).toList,  e._1(2).split(" ").map(_.toInt).toList   ) 
-    println("Case #"+(e._2+1)+": "+solution)
+  groups.zipWithIndex foreach { e =>  
+       val solution = minScalarProduct ( e._1(1).split(" ").map(_.toInt).toList,  e._1(2).split(" ").map(_.toInt).toList   ) 
+       println("Case #"+(e._2+1)+": "+solution)
+  }
 }
-}
-
-processInput(io.Source.fromFile("minScalarProduct.in"))
+//input from the file input.txt
+processInput(io.Source.fromFile("input.txt"))

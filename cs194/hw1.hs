c1e1a7f8b8d6165af39266cf194c9c794ad3282b
Abhishek Kona <abhishek.kona@gmail.com>
2018-05-14 20:55:29 -0700

    homework

--- cs194/hw1.hs
+++ cs194/hw1.hs
@@ -8,3 +8,23 @@ revList (x:xs) = (revList xs) ++ [x]
 
 toDigits :: Integer -> [Integer]
 toDigits n  = revList(toDigitsRev n)
+
+-- doubleHelper:: [Integer] -> Bool -> [Integer]
+-- doubleHelper []  _ = []
+-- doubleHelper (x:xs) True = 2*x : doubleHelper(xs, False)
+-- doubleHelper (x:xs) False = x : doubleHelper(xs, True)
+--
+doubleLeft :: [Integer] -> [Integer]
+doubleLeft [] = []
+doubleLeft [x] = [x]
+doubleLeft (x:y:xs) = x: 2*y : doubleLeft(xs)
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther l = revList ( doubleLeft (revList l))
+
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x:xs) = (mod x 10) + (div x 10) + (sumDigits xs)
+
+validate :: Integer -> Bool
+validate n = (mod (sumDigits (doubleEveryOther (toDigits n))) 10) == 0

c6786c69e4637113f0e5f9751c21c62bd320886b
Abhishek Kona <abhishek.kona@gmail.com>
2018-05-31 20:24:15 -0700

    done

--- cs194/hw3.hs
+++ cs194/hw3.hs
@@ -1,3 +1,4 @@
+import Data.List
 takeN :: [a] -> Int -> [a] 
 takeN (x:xs) n = x : (takeN (drop (n-1) xs) n) 
 takeN [] _ = []
@@ -8,4 +9,15 @@ skips l = let
   skipHelp n = takeN (drop (n - 1) l) n
   in map skipHelp [1..(length l)]
 
-localMaxima :: [Integer] -> [Integer]
+localMaxima :: [Integer] -> [Integer] 
+localMaxima [] = []
+localMaxima l@(a:b:c:xs) 
+  | a<b && b > c = [b] ++ (localMaxima (drop 1 l))
+localMaxima l = (localMaxima (drop 1 l))
+
+histogram :: [Integer] -> String
+histogram arr = let
+  cntArr = map (\n -> length (filter (\x -> x == n) arr)) [1..9]
+  maxSize = maximum cntArr
+  row n = map (\x -> if x >= n then '*' else ' ')  cntArr 
+  in (intercalate "\n" (map row (reverse [1..maxSize]))) ++ "\n==========\n0123456789\n"

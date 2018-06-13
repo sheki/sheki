12285df11dcb7d61673d3c1a2f52f36566f7cc6a
Abhishek Kona <abhishek.kona@gmail.com>
2018-06-12 21:20:25 -0700

    Getting a hang of the compose `.` operator

---
+++ cs194/hw4.hs
@@ -0,0 +1,16 @@
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs) 
+  | even x = (x - 2) * fun1 xs
+  | otherwise = fun1 xs
+
+fun1' :: [Integer] -> Integer
+fun1' = product . map (\x -> x -2). filter even
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n | even n = n + fun2 (n `div` 2)
+  | otherwise = fun2 (3 * n + 1)
+
+fun2' :: Integer -> Integer
+fun2'  = sum . filter even . takeWhile (> 1) . iterate (\x-> if even x then x `div` 2 else 3 * x + 1)

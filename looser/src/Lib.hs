a984380ee062a1a07e58ec21951ff0be72275541
Abhishek Kona <abhishek.kona@gmail.com>
2018-08-27 21:50:14 -0700

    woot woot

--- looser/src/Lib.hs
+++ looser/src/Lib.hs
@@ -3,18 +3,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Lib
-    ( parseStocks
-    ) where
+  ( parseStocks
+  )
+where
 
-import Data.List
-import GHC.Generics
-import Data.Aeson
-import Data.Scientific
-import Control.Lens
-import Network.Wreq
+import           Data.List
+import           GHC.Generics
+import           Data.Aeson
+import           Data.Scientific
+import           Control.Lens
+import           Network.Wreq
 
 parseTicker :: String -> String
-parseTicker = takeWhile (\x -> x /= ',') 
+parseTicker = takeWhile (\x -> x /= ',')
 
 data Price = Price {
   price :: Scientific,
@@ -22,7 +23,7 @@ data Price = Price {
 } deriving (Generic, Show, ToJSON, FromJSON)
 
 doublePrice :: Price -> Double
-doublePrice  x = toRealFloat (price x)
+doublePrice x = toRealFloat (price x)
 
 data APIResponse = APIResponse {
   open :: Price,
@@ -32,24 +33,24 @@ data APIResponse = APIResponse {
 } deriving (Generic, Show, ToJSON, FromJSON)
 
 generateURL :: String -> String
-generateURL x = "https://api.iextrading.com/1.0/stock/"++x++"/ohlc"
+generateURL x = "https://api.iextrading.com/1.0/stock/" ++ x ++ "/ohlc"
 
 delta a = (doublePrice (close a)) - (doublePrice (open a))
 
 deltaPercent :: APIResponse -> Double
 deltaPercent a = 100.0 * (delta a) / (doublePrice (open a))
 
-generateMsgStr ::String -> APIResponse -> String
+generateMsgStr :: String -> APIResponse -> String
 generateMsgStr sym x = sym ++ " " ++ (show (deltaPercent x))
 
-getStockPrice :: String -> IO()
+getStockPrice :: String -> IO ()
 getStockPrice sym = do
   r <- asJSON =<< get (generateURL sym)
-  let resp = (r ^. responseBody) 
-  let msg = generateMsgStr sym resp
+  let resp = (r ^. responseBody)
+  let msg  = generateMsgStr sym resp
   putStrLn msg
 
-getTickers = map parseTicker . lines 
+getTickers = map parseTicker . lines
 
-parseStocks :: String -> [IO()]
+parseStocks :: String -> [IO ()]
 parseStocks = map getStockPrice . getTickers

57987b796e655fa5cdbf8386f989d394b1a43ae2
Abhishek Kona <akona@slack-corp.com>
2020-03-05 19:01:35 -0500

    Checkpoint

--- what/src/App.js
+++ what/src/App.js
@@ -1,6 +1,16 @@
 import React from "react";
-import "./debug.css";
 import Styles from "./App.module.css";
+
+function Button(props) {
+  return (
+    <a
+      className="f3 link br3 dim ph3 pv2 mb2 dib white bg-dark-blue"
+      href={props.href}
+    >
+      {props.text}
+    </a>
+  );
+}
 
 function App() {
   return (
@@ -9,10 +19,20 @@ function App() {
         <div className="bg-washed-red">
           <div className={"bg-washed-red " + Styles.hero}>
             <h1 className="f-headline">[NAME]</h1>
-            <h3 className="f-subheadline">We do awesome things.</h3>
+            <h3 className="f1">You live the experience. We do the work.</h3>
+            <Button href="#" text="Take a trip" />
           </div>
         </div>
       </header>
+      <div
+        className={
+          "ba bw3 flex flex-column items-center justify-center " +
+          Styles.image1Parent
+        }
+      >
+        <h3 className="f1">Let the party begin</h3>
+        <Button href="#" text="Make us work" />
+      </div>
       <footer className="footer pv4 ph3 ph5-m ph6-l mid-gray">
         <small className="f6 db tc">
           © 2019 <b className="ttu">Irving Place inc.</b>., All Rights Reserved
